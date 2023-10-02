3cf034c308a5ba9dbd028190cdcf4bb7d5bd0d23
AlexandrZolotukhin <zoloto.alex08@yandex.ru>
2023-10-02 21:24:56 +0300

    Add project files.

---
+++ IDA_Class_Work_5/!_Main.cpp
@@ -0,0 +1,50 @@
+﻿
+// Class Work 02.10.2023 -------------------------------------------	
+
+
+/// CRUDs commands
+/*
+CREATE
+READ (REQUEST)
+UPDATE
+DELETE (DROP)
+*/
+
+
+  /////////////////////
+ /// DML операторы ///
+/////////////////////
+/*
+INSERT - создание записи
+SELECT - получение текущих данных
+UPDATE - изменение текущих данных
+DELETE - удаление данных
+*/
+
+/// INSERT
+/*
+INSERT {* | filed1, field2, field3} INTO TABLE_NAME (value1, value2, value3)
+
+
+Если INSERT * - имеет значение порядок полей
+Если INSERT field1, field2 - порядок полей определен в запросе
+*/
+
+/// DELETE
+/*
+
+DELETE INTO TABLE_NAME
+WHERE id = 4
+
+*/
+
+/// UPDATE
+/*
+UPDATE {* | field1,field2} FROM TABLE_NAME
+WHERE id = 4 (value1,value2)
+*/
+
+/// SELECT
+/*
+SELECT {* | field1 [as 'nickname'], field2, field3+field4[as 'Zarplata']} FROM TABLE_NAME []
+*/
